88f7635306c2e736c4c251dd25c58ebaf5ec4691
Hai Nguyen <hainm.comp@gmail.com>
2014-09-12 16:03:22 -0400

    adding checking class

---
+++ GB/CheckInput.py
@@ -0,0 +1,10 @@
+import sys
+
+class CheckInput:
+    def check_igb(self,igb):
+        if igb not in [1, 2, 5, 7, 8]:
+            print "igb must be 1, 2, 5, 7 or 8"
+            sys.exit()
+    def check_top_crd(self,mol):
+        '''check whether topology and crd files have the same NATOMS'''
+        pass
