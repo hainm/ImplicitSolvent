74532b90b145c57085a9ae1dac8d05420c6baa9f
Hai Nguyen <hainm.comp@gmail.com>
2014-09-12 17:57:37 -0400

    adupdte paperInfo.py

--- GB/paperInfo.py
+++ GB/paperInfo.py
@@ -4,9 +4,12 @@ class PaperInfo():
     DESCRITION_IGB='''calculate solvation energy using igb=1,2,5,7,8 in AMBER
     '''
 
-    GBHCT = '''Hawkins, G. D.; Cramer, C. J.; Truhlar, D. G., Pairwise solute descreening of solute charges from a dielectric medium. Chem. Phys. Lett. 1995, 246 (1-2), 122-129.'''
-    GBOBC = '''Onufriev, A.; Bashford, D.; Case, D. A., Exploring protein native states and large-scale conformational changes with a modified generalized born model.Proteins: Struct., Funct., Bioinf. 2004, 55 (2), 383-394.'''
-    GBNeck = '''Mongan, J.; Simmerling, C.; McCammon, J. A.; Case, D. A.; Onufriev, A., Generalized Born Model with a Simple, Robust Molecular Volume Correction. J. Chem. Theory Comput. 2007, 3 (1), 156-169.'''
+    GBHCT = '''
+    Hawkins, G. D.; Cramer, C. J.; Truhlar, D. G., Pairwise solute descreening of solute charges from a dielectric medium. Chem. Phys. Lett. 1995, 246 (1-2), 122-129.'''
+    GBOBC = '''
+    Onufriev, A.; Bashford, D.; Case, D. A., Exploring protein native states and large-scale conformational changes with a modified generalized born model.Proteins: Struct., Funct., Bioinf. 2004, 55 (2), 383-394.'''
+    GBNeck = '''
+    Mongan, J.; Simmerling, C.; McCammon, J. A.; Case, D. A.; Onufriev, A., Generalized Born Model with a Simple, Robust Molecular Volume Correction. J. Chem. Theory Comput. 2007, 3 (1), 156-169.'''
     GBNeck2_GBNecknu2='''
     1. Protein
     Nguyen, H.; Roe, D. R.; Simmerling, C., Improved Generalized Born Solvent Model Parameters for Protein Simulations. J. Chem. Theory Comput. 2013, 9 (4), 2020-2034.
@@ -20,14 +23,15 @@ class PaperInfo():
             self.printInfo(self.igb)
     def printInfo(self,igb):
         if igb == 1:
-            print self.GBHCT
+            print "\nIGB = %s %s " %(igb, self.GBHCT)
         elif igb in [2,5]:
-            print self.GBOBC
+            print "\nIGB = %s %s " %(igb, self.GBOBC)
         elif igb == 7:
-            print self.GBNeck
+            print "\nIGB = %s %s " %(igb, self.GBNeck)
         elif igb == 8:
-            print self.GBNeck2_GBNecknu2
+            print "\nIGB = %s %s " %(igb, self.GBNeck2_GBNecknu2)
 
 if __name__ == '__main__':
-    info = PaperInfo(8)
-#    info.printInfo(8)
+    info = PaperInfo()
+    for igb in [1, 2, 5, 7, 8]:
+        info.printInfo(igb)
