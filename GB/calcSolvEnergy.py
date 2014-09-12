0f7993d7274554607952cdff6b1e8b4e42d060aa
Hai Nguyen <hainm.comp@gmail.com>
2014-09-12 16:21:02 -0400

    update calcSolv:

--- GB/calcSolvEnergy.py
+++ GB/calcSolvEnergy.py
@@ -1,3 +1,15 @@
 '''calculate solvation energy using igb=1,2,5,7,8 in AMBER
 usage = python this_script --igb=<igb> --top=top --crd=crd [--pdb=pdb]
 '''
+from CheckInput import *
+from GBClass import Molecule
+
+check = CheckInput()
+check.check_igb(igb)
+check.check_top_crd(top,crd)
+
+mol = Molecule(top=top,crd=crd,pbd=pdb)
+
+print mol.get_solv(igb=igb)
+
+
