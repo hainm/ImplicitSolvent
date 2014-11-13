ed69b73af1c46686767113e6e1c6fea543bff6e3
hainm <hainm.comp@gmail.com>
2014-11-13 14:30:44 -0500

    follow PEP8 style

--- GB/Molecule2.py
+++ GB/Molecule2.py
@@ -1,8 +1,8 @@
 from GBClass import Molecule
 import re
 
-#parse_file function was copied and modified from
-#https://github.com/pele-python/pele
+# parse_file function was copied and modified from
+# https://github.com/pele-python/pele
 data_casts = {"a": str,
               "A": str,
               "e": float,
@@ -12,10 +12,10 @@ data_casts = {"a": str,
 
 
 class Molecule2(Molecule):
-    def __init__(self,top='',crd=''):
-        Molecule.__init__(self,top='',crd='')
-        self.topology_data = {}
 
+    def __init__(self, top='', crd=''):
+        Molecule.__init__(self, top='', crd='')
+        self.topology_data = {}
 
     def parse_file(self, fname):
         '''this parse_file was copied (and modified) from above link'''
@@ -24,22 +24,24 @@ class Molecule2(Molecule):
         data_length = None
         data_type = None
 
-        lines = open(fname,'r').readlines()
+        lines = open(fname, 'r').readlines()
         for line in lines:
             # Remove whitespace and newlines
             line = line.rstrip()
-            if line.startswith("%VERSION"): continue
+            if line.startswith("%VERSION"):
+                continue
             elif line.startswith("%FLAG"):
                 flag = line[6:]
-                #AMBER topology file uses upper case for flag
-                #I prefer lower case
+                # AMBER topology file uses upper case for flag
+                # I prefer lower case
                 self.topology_data[flag.lower()] = []
             elif line.startswith("%FORMAT"):
-                format      = line[8:-1]
+                format = line[8:-1]
                 data_length = int(re.split("[aEI\.\)]", format)[1])
-                data_type   = re.findall("[a-zA-Z]", format)[0]
+                data_type = re.findall("[a-zA-Z]", format)[0]
             else:
-                split_line = [line[i:i+data_length] for i in range(0, len(line), data_length)]
+                split_line = [line[i:i + data_length]
+                              for i in range(0, len(line), data_length)]
                 formatted_line = map(data_casts[data_type], split_line)
                 self.topology_data[flag.lower()] += formatted_line
         return self.topology_data
@@ -49,4 +51,3 @@ if __name__ == '__main__':
     crd = "Tc5b.nat.crd"
     mol = Molecule(top=top, crd=crd)
     print mol.atoms[0].xyz
-
--- GB/paperInfo.py
+++ GB/paperInfo.py
@@ -8,10 +8,11 @@ It would be great if you can give me credit for my work.
     Nguyen, H.; Perez, A.; Simmerling, C., Refinement of Generalized-Born Neck Parameters for Nucleic Acid and Their Complex with Protein. (in preperation).
 '''
 
+
 class PaperInfo():
     HELPIGB = '''igb = 1, 2, 5, 7, or 8'''
 
-    DESCRITION_IGB='''calculate solvation energy using igb=1,2,5,7,8 in AMBER
+    DESCRITION_IGB = '''calculate solvation energy using igb=1,2,5,7,8 in AMBER
     '''
 
     GBHCT = '''
@@ -20,7 +21,7 @@ class PaperInfo():
     Onufriev, A.; Bashford, D.; Case, D. A., Exploring protein native states and large-scale conformational changes with a modified generalized born model.Proteins: Struct., Funct., Bioinf. 2004, 55 (2), 383-394.'''
     GBNeck = '''
     Mongan, J.; Simmerling, C.; McCammon, J. A.; Case, D. A.; Onufriev, A., Generalized Born Model with a Simple, Robust Molecular Volume Correction. J. Chem. Theory Comput. 2007, 3 (1), 156-169.'''
-    GBNeck2_GBNeck2nu='''
+    GBNeck2_GBNeck2nu = '''
     1. Protein
     Nguyen, H.; Roe, D. R.; Simmerling, C., Improved Generalized Born Solvent Model Parameters for Protein Simulations. J. Chem. Theory Comput. 2013, 9 (4), 2020-2034.
 
@@ -28,17 +29,18 @@ class PaperInfo():
     Nguyen, H.; Perez, A.; Simmerling, C., Refinement of Generalized-Born Neck Parameters for Nucleic Acid and Their Complex with Protein. (in preperation).
     '''
 
-    def __init__(self,igb=''):
+    def __init__(self, igb=''):
         self.igb = igb
         if self.igb:
             self.printInfo(self.igb)
-    def printInfo(self,igb):
-        IGBDICT = {'1':self.GBHCT,
-                   '2':self.GBOBC,
-                   '5':self.GBOBC,
-                   '7':self.GBNeck,
-                   '8':self.GBNeck2_GBNeck2nu}
-        print "\nIGB = %s %s " %(igb, IGBDICT[str(igb)])
+
+    def printInfo(self, igb):
+        IGBDICT = {'1': self.GBHCT,
+                   '2': self.GBOBC,
+                   '5': self.GBOBC,
+                   '7': self.GBNeck,
+                   '8': self.GBNeck2_GBNeck2nu}
+        print "\nIGB = %s %s " % (igb, IGBDICT[str(igb)])
 
 if __name__ == '__main__':
     info = PaperInfo()
